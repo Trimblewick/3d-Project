bd98a85ddea5b0627510413a0d45ce7524d7dac2
Trimblewick <gustav.ekelund@hotmail.com>
2018-02-19 23:52:43 +0100

    further work on gpuhighway

--- DV2551-Project/GPUHighway.cpp
+++ DV2551-Project/GPUHighway.cpp
@@ -2,17 +2,54 @@
 #include "GPUHighway.h"
 
 
-GPUHighway::GPUHighway(ID3D12CommandQueue * pCQ, ID3D12CommandAllocator ** ppCAs, unsigned int iNumberOfCAs, ID3D12GraphicsCommandList ** ppCLs, unsigned int iNumberOfCLs)
+GPUHighway::GPUHighway(
+	D3D12_COMMAND_LIST_TYPE			type, 
+	ID3D12CommandQueue*				pCQ,
+	ID3D12CommandAllocator**		ppCAs, 
+	ID3D12Fence**					ppFences, 
+	unsigned int					iNumberOfCAsAndFences, 
+	ID3D12GraphicsCommandList**		ppCLs, 
+	unsigned int					iNumberOfCLs)
 {
+	assert(iNumberOfCAsAndFences >= iNumberOfCLs);
+	m_type = type;
 	m_pCQ = pCQ;
+	
 	m_ppCAs = ppCAs;
-	m_iNumberOfCAs = iNumberOfCAs;
+	m_ppFences = ppFences;
+	m_iNumberOfCAsAndFences = iNumberOfCAsAndFences;
+
+	m_pFenceValues = new size_t[iNumberOfCAsAndFences];
+	m_ppCAVec = new std::vector<ID3D12CommandAllocator*>[iNumberOfCAsAndFences];
+	m_pFenceLocked = new bool[iNumberOfCAsAndFences];
+
+	m_handleFence = CreateEvent(NULL, NULL, NULL, NULL);
+
+	for (int i = 0; i < iNumberOfCAsAndFences; ++i)
+	{
+		m_pFenceValues[i] = 0;
+		m_pFenceLocked[i] = false;
+		
+	}
+
 	m_ppCLs = ppCLs;
 	m_iNumberOfCLs = iNumberOfCLs;
+	m_pCLLocked = new bool[iNumberOfCLs];
+	for (int i = 0; i < iNumberOfCLs; ++i)
+	{
+		m_pCLLocked = false;
+	}
 }
 
 GPUHighway::~GPUHighway()
 {
+	delete m_pFenceValues;
+
+	for (int i = 0; i < m_iNumberOfCAsAndFences; ++i)
+	{
+		
+	}
+
 }
 
 ID3D12CommandQueue * GPUHighway::GetCQ()
@@ -20,15 +57,60 @@ ID3D12CommandQueue * GPUHighway::GetCQ()
 	return m_pCQ;
 }
 
-ID3D12GraphicsCommandList * GPUHighway::GetFreeCL()
+void GPUHighway::QueueCL(ID3D12CommandList* pCL)
+{
+	m_ppCLQ.push_back(pCL);
+}
+
+ID3D12GraphicsCommandList * GPUHighway::GetFreshCL()
 {
 	return nullptr;
 }
 
-void GPUHighway::ExecuteCQ(ID3D12Fence* pFence, const size_t iValue)
+int GPUHighway::ExecuteCQ()
 {
 	m_pCQ->ExecuteCommandLists(m_ppCLQ.size(), m_ppCLQ.data());
 	m_ppCLQ.clear();
 
-	m_pCQ->Signal(pFence, iValue);
+	int index = -1;
+	for (int i = 0; i < m_iNumberOfCAsAndFences; ++i)//use the first fence that is availible
+	{ 
+		if (!m_pFenceLocked[i])
+		{
+			m_pCQ->Signal(m_ppFences[i], m_pFenceValues[i]);
+			index = i;
+			i = m_iNumberOfCAsAndFences;
+		}
+	}
+	return index;
 }
+
+void GPUHighway::Wait(int index)
+{
+	if (!m_pFenceLocked)
+		return;
+
+	if (m_ppFences[index]->GetCompletedValue() < m_pFenceValues[index])
+	{
+		m_ppFences[index]->SetEventOnCompletion(m_pFenceValues[index], m_handleFence);
+		WaitForSingleObject(m_handleFence, INFINITE);
+	}
+	m_pFenceValues[index]++;
+	m_pFenceLocked[index] = false;
+
+	//reset CAs
+	DxAssert(m_ppCAs[index]->Reset());
+}
+
+void GPUHighway::WaitForAllFences()
+{
+	for (int i = 0; i < m_iNumberOfCAsAndFences; ++i)
+	{
+		if (m_pFenceLocked[i])
+		{
+			Wait(i);
+		}
+	}
+}
+
+
--- DV2551-Project/GPUHighway.h
+++ DV2551-Project/GPUHighway.h
@@ -6,24 +6,37 @@
 class GPUHighway
 {
 public:
-	GPUHighway(ID3D12CommandQueue* pCQ, ID3D12CommandAllocator** ppCAs, unsigned int iNumberOfCAs, ID3D12GraphicsCommandList** ppCLs, unsigned int iNumberOfCLs);
+	GPUHighway(D3D12_COMMAND_LIST_TYPE	type, ID3D12CommandQueue * pCQ, ID3D12CommandAllocator ** ppCAs, ID3D12Fence** ppFences, unsigned int iNumberOfCAsAndFences, ID3D12GraphicsCommandList ** ppCLs, unsigned int iNumberOfCLs);
 	~GPUHighway();
 
 	ID3D12CommandQueue*						GetCQ();
 
-	ID3D12GraphicsCommandList*				GetFreeCL();
+	void									QueueCL(ID3D12CommandList* pCL);//Make sure only to queue commandlists on the same highway
+	ID3D12GraphicsCommandList*				GetFreshCL();
 	
-	void									ExecuteCQ(ID3D12Fence* pFence, const size_t iValue);
+	int										ExecuteCQ();//Execute all queued commandlists. Returns the index of the fence used.
+	void									Wait(int index);//Wait for a particular fence using a index. Returns -1 if no free fences was found.
+	void									WaitForAllFences();//Full reset, all fences will be waited for.
+
 private:
 	D3D12_COMMAND_LIST_TYPE					m_type;
 	
 	ID3D12CommandQueue*						m_pCQ;
 
 	ID3D12CommandAllocator**				m_ppCAs;
-	unsigned int							m_iNumberOfCAs; 
+	std::vector<ID3D12CommandAllocator*>*	m_ppCAVec;
+	ID3D12Fence**							m_ppFences;
+	unsigned int							m_iNumberOfCAsAndFences; 
+	size_t*									m_pFenceValues;
+	bool*									m_pFenceLocked;
+	HANDLE									m_handleFence;
+	
+
 	ID3D12GraphicsCommandList**				m_ppCLs;
 	unsigned int							m_iNumberOfCLs;
+	bool*									m_pCLLocked;
 
 	std::vector<ID3D12CommandList*>			m_ppCLQ;
+	
 };
 
