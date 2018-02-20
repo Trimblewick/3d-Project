bb01a1a9649cf4423da1b7d54bd57d6529f13b60
U-STUDENTS\niwc13 <niwc13@H473-0001.students.ad.bth.se>
2018-02-20 11:45:19 +0100

    Started implementing basic Root Constant info and removed vcxproj files from git

--- DV2551-Project/BezierClass.cpp
+++ DV2551-Project/BezierClass.cpp
@@ -1,5 +1,37 @@
+#include "stdafx.h"
 #include "BezierClass.h"
+
+void BezierClass::SetRootDesc()
+{
+	//fill root desc, create input and register spaces etc..
+
+	//Create one rp for the color root constant
+	D3D12_ROOT_PARAMETER rp[1];
+
+	//Color
+	D3D12_ROOT_CONSTANTS colorConstant;
+	colorConstant.Num32BitValues = 4;
+	colorConstant.ShaderRegister = 0; //set color as b0?
+	colorConstant.RegisterSpace = 0;
+
+	rp[0].Constants = colorConstant;
+	rp[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS;
+	rp[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+
+	float4 color;// = float4(1, 0.0f, 0.0f, 1.0f);
+	color.x = 1.0f;
+	color.y = 0.0f;
+	color.z = 0.0f;
+	color.w = 1.0f;
+
+	//commandList->SetGraphicsRoot32BitConstants(color etc..);
+	//commandList->DrawInstanced();
+
+	//createRS()??
+
+}
 
 void BezierClass::CalculateBezierVertices()
 {
+	return;
 }
--- DV2551-Project/BezierClass.h
+++ DV2551-Project/BezierClass.h
@@ -1,14 +1,29 @@
 #pragma once
-#include "stdafx.h"
+#include <d3d12.h>
+#include <d3dcompiler.h>
+#include <assert.h>
+
+//#include "stdafx.h"
 //calculates Bezier
+struct float4
+{
+	float x;
+	float y;
+	float z;
+	float w;
+};
+
 class BezierClass
 {
 public:
-
+	//public functions
+	void SetRootDesc();
 
 private:
+	//private functions
 	void CalculateBezierVertices();
-
 	
 
+	//private variables
+	std::vector<float4> m_pBezierVertices;
 };
