b112483af809172dffcbcfa49925863a347d42a1
Oskar Warnhag <oskar.warnhag@gmail.com>
2018-03-08 14:13:59 +0100

    Replaced d3dx12's updatesubresources with my own

--- DV2551-Project/D3DFactory.h
+++ DV2551-Project/D3DFactory.h
@@ -27,11 +27,11 @@ public:
 	ID3D12DescriptorHeap*					CreateDH(int numDescriptors, D3D12_DESCRIPTOR_HEAP_TYPE type, bool bShaderVisible);
 	ID3DBlob*								CompileShader(LPCWSTR filePath, LPCSTR shadermodel);
 	ID3D12Resource*							CreateCommitedResource(D3D12_HEAP_PROPERTIES* pHeapProperties, D3D12_RESOURCE_DESC* pResourceDesc, D3D12_RESOURCE_STATES initState);
+	void									UpdateSubresources(ID3D12GraphicsCommandList* pCmdList, ID3D12Resource* pBufferHeap, ID3D12Resource* pUploadHeap, D3D12_SUBRESOURCE_DATA* pData);
 	GPUHighway*								CreateGPUHighway(D3D12_COMMAND_LIST_TYPE type, unsigned int iNumberOfCLs);
 	Camera*									CreateCamera(unsigned int iBufferCount, long iWidthWindow, long iHeightWindow);
 	BezierClass*							CreateBezier(int nrOfVertices);
 	Plane*									CreatePlane(ID3D12GraphicsCommandList* pCmdList, unsigned int tiles, ID3D12Resource* pUploadHeapVertexBuffer, ID3D12Resource* pUploadHeapIndexBuffer);
-	Plane*									CreatePlane(ID3D12GraphicsCommandList* pCmdList, unsigned int tiles, int alt);
 
 private:
 	IDXGIFactory4*							m_pDXGIFactory;
