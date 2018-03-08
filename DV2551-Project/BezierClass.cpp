e9f67c7e22585ec0ab505be6f85914797fcd93a4
Trimblewick <gustav.ekelund@hotmail.com>
2018-03-08 09:28:29 +0100

    Bezier

--- DV2551-Project/BezierClass.cpp
+++ DV2551-Project/BezierClass.cpp
@@ -47,6 +47,7 @@ void BezierClass::CalculateBezierPoints(int width/*, int x, int y*/)
 					temp.x = grid*j/* + width * x*/;
 					temp.y = rand() % 20 - 10;
 					temp.z = grid * i/* + width * y*/;
+					temp.w = 1.0f;
 
 					m_pBezierPoints.push_back(temp);
 				}
@@ -60,11 +61,11 @@ void BezierClass::UpdateBezierPoints()
 {
 	for (int i = 0; i < m_pBezierPoints.size(); ++i)
 	{
-		//m_pBezierPoints[i].y = rand() % 20 - 10; //change to a random Y factor, something like comment below this line
-		m_pBezierPoints[i].y = m_pBezierPoints[i].y;//random value between ??? 0 and 10???
+		m_pBezierPoints[i].y = m_pBezierPoints[i].y + (rand() % 20 - 10)/100.0f; //change to a random Y factor, something like comment below this line
+		//m_pBezierPoints[i].y = m_pBezierPoints[i].y;//random value between ??? 0 and 10???
 	}
 
-	//memcpy(m_address, reinterpret_cast<void*>(&m_pBezierPoints), m_nrOfVertices * sizeof(float4));
+	memcpy(m_address, m_pBezierPoints.data(), m_nrOfVertices * sizeof(float4));
 }
 
 void BezierClass::BindBezier(ID3D12GraphicsCommandList * pCL, unsigned int iBufferIndex)
